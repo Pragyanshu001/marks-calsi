d5826506c06eeb9d49ec92e0e47c0728cec47dc1
Pragyanshu Gupta <pragyanshugupta001@gmail.com>
2026-01-23 20:26:45 +0530

    back to normal

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://marks-calsi.onrender.com", "http://localhost:5173"],
+    origin: ["https://marks-calsi-1.onrender.com"],
     credentials: true,
   }),
 );
