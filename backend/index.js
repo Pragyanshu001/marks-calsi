721cfba1d338df8be4a5485bacca728219413e3e
Pragyanshu Gupta <pragyanshugupta001@gmail.com>
2026-01-23 20:23:23 +0530

    backend url

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://marks-calsi-1.onrender.com"],
+    origin: ["https://marks-calsi.onrender.com", "http://localhost:5173"],
     credentials: true,
   }),
 );
